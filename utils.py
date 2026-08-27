2465d0b5bb846320ac618951c3cea296b125efce
okeylooft <tr11ck55terr@gmail.com>
2026-08-27 20:08:53 +0500

    Добавил файл, в котором будут функции которые сложно отнестести к одному из других файлов

---
+++ utils.py
@@ -0,0 +1,15 @@
+def print_header_SLA():
+    print(50 * "=")
+    print("SLA MONITOR")
+
+
+def print_menu():
+    print(50 * "=")
+    print()
+    print("1. Create ticket")
+    print("2. Show ticket")
+    print("3. Search ticket")
+    print("4. Change status")
+    print("5. Delete ticket")
+    print("6. Check SLA")
+    print("7. Exit")
