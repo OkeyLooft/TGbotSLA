7dec3d1906bc8a0a88e3d9f6ebea0beae10e7eec
okeylooft <tr11ck55terr@gmail.com>
2026-08-28 17:07:08 +0500

    Добавил проверку перед записью в бд

--- handlers.py
+++ handlers.py
@@ -21,10 +21,34 @@ class TicketHandlers:
         
         return name, description, sla
 
+
+
     def create_ticket(self):
-        name, description, sla = self.input_ticket()
+        while True:
+            name, description, sla = self.input_ticket()
+
+            ticket_dict: dict = {
+                "NAME": name,
+                "DESCRIPTION": description,
+                "SLA": sla
+            }
+
+            for keys, items in ticket_dict.items():
+                print(keys, items)
+            
+            user_agree: str = input("Все верно ? [д/н] ").lower()
+            if user_agree == "д":
+                break
+            elif user_agree == "н":
+                pass
+            else:
+                print("Проверьте что ввели 'д' или 'н' ")
+
         self.service.create_ticket(
             name=name,
             description=description,
             sla=sla
         )
+
+ticket = TicketHandlers(TicketServices(TicketStorage()))
+ticket.create_ticket()
