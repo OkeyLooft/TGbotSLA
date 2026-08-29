d5031f9c6eadc5db31b1ec05e2921691ecb3f03d
okeylooft <tr11ck55terr@gmail.com>
2026-08-29 20:10:12 +0500

    Сделал возможность показа тикетов, так же реализовал возомжность поиска тикетов через меню

--- handlers.py
+++ handlers.py
@@ -1,11 +1,12 @@
+from datetime import datetime, timedelta, timezone
+
+import utils
 from services import TicketServices
-from storage import TicketStorage
 
 
 class TicketHandlers:
     def __init__(self, services: TicketServices):
         self.service = services
-
 
     def input_ticket(self):
         name: str = input("Задайте имя тикету: ")
@@ -27,7 +28,7 @@ class TicketHandlers:
         while True:
             name, description, sla = self.input_ticket()
 
-            ticket_dict: dict = {
+            ticket_dict: dict = {   
                 "NAME": name,
                 "DESCRIPTION": description,
                 "SLA": sla
@@ -49,3 +50,77 @@ class TicketHandlers:
             description=description,
             sla=sla
         )
+
+    def show_ticket(self) -> None:
+        utils.show_ticket_header()
+        data: list = self.service.show_ticket()
+        for d in data:
+            id_: int = d["id"]
+            name: str = d["name"]
+            status: str = d["status"]
+            hours_left: timedelta = d["hours_left"]
+            print(f"{id_:<3}|{name:<15}|{status:<13}|{hours_left}")
+
+
+    def choice_find_status(self) -> str:
+        status_menu: dict = {
+            "1": "OPEN",
+            "2": "IN_PROGRESS",
+            "3": "CLOSED",
+            "4": "EXPIRED",
+            "5": "Exit"
+        }
+        while True:
+            print("1. OPEN\n2. IN_PROGRESS\n3. CLOSED\n4. EXPIRED\n5. Exit")
+            choice_status: str = input("Введите номер варианта из меню: ")
+            choice_status_find: str = status_menu.get(choice_status)
+            if choice_status_find:
+                status_find: str = choice_status_find
+                break
+            else:
+                print("Проверьте что ввели правильный номер варианта.")
+        return status_find
+
+    
+    def choice_find_menu(self) -> str:
+        menu: dict = {
+            "1": "ID",
+            "2": "NAME",
+            "3": "STATUS",
+            "4": "Exit"
+        }
+        while True:
+            utils.menu_find_ticket()
+            choice_menu: str = input("Введите номер варианта поиска: ")
+            choice_menu_find: str = menu.get(choice_menu)
+            if choice_menu_find:
+                menu_find:str = choice_menu_find
+                break
+            else:
+                print("Проверьте что ввели правильный номер варианта.")
+        return menu_find
+                    
+    
+    def find_ticket_to_service(self):
+        while True:
+            menu_find:str = self.choice_find_menu()
+            if menu_find == "Exit":
+                break
+            elif menu_find == "STATUS":
+                while True:
+                    status_find: str = self.choice_find_status()
+                    if status_find == "Exit":
+                        break
+                    else:
+                        value: str = input
+                        self.service.find_ticket(
+                            menu_find=status_find
+                        )
+            else:
+                value: str = input(f"Введите {menu_find}")
+                self.service.find_ticket(
+                    menu_find=menu_find,
+                    value=value
+                )
+        
+
