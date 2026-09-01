f0812447b85c982feb2647f3bf85e354969995fb
okeylooft <tr11ck55terr@gmail.com>
2026-09-01 21:25:41 +0500

    Заполнил мейн до работоспособного состояния

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ def main():
     actions: dict = {
         "1": ticket.create_ticket,
         "2": ticket.show_ticket,
-        "3": ticket.find_ticket
+        "3": ticket.find_ticket,
+        "4": ticket.change_ticket,
+        "5": ticket.delete_ticket,
+        "6": ticket.view_sla
 }   
-    
+    while True:
+        utils.print_menu()
+        choice: str = input("Выберите вариант из меню: ")
+        if choice == "7":
+            break
+        action = actions.get(choice)
+        if action:
+            action()
+            input("Нажмите любую кнопку что бы продолжить...")
+        else:
+            print("Проверьте что ввели верный номер из меню.") 
+
+if __name__ == "__main__":
+    main()   
